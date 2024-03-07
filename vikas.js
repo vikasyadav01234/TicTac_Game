0761c80d118f162ef87d143373793938cdb418db
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-07 11:17:07 +0530

    main

--- vikas.js
+++ vikas.js
@@ -1,3 +1,3 @@
 const boxes = document.querySelectorAll(".box");
 const gameInfo = document.querySelectorAll(".game-info");
-const newGameBtn = document.querySelectorAll(".btn")
+const newGameBtn = document.querySelectorAll(".btn");
