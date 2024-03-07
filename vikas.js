0615bdc0faf976c0da78d7e26ac30d3336f901e5
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-07 11:28:12 +0530

    main

--- vikas.js
+++ vikas.js
@@ -4,3 +4,15 @@ const newGameBtn = document.querySelectorAll(".btn");
 
 let currentPlayer;
 let gameGrid;
+
+const winningPositions =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
